312aaa4a5db8fe60e2f09cb7c18cf4abe4a1ab4f
Manmay <manmay.e.mohanty@gmail.com>
2017-06-29 22:40:00 +0200

    Update sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+insert into grant_types(grant) values('password');
+insert into grant_types(grant) values('implicit');
+insert into grant_types(grant) values('authorization_code');
 insert into grant_types(grant) values('client_credentials');
 
 insert into authorities(role) values('admin');
@@ -7,6 +10,7 @@ insert into authorities(role) values('developer');
 
 insert into authorities(role) values('notification_user');
 insert into authorities(role) values('notification_admin');
+
 insert into authorities(role) values('calendar_admin');
 
 insert into users(id, password, enabled) values('admin', 'admin', true);
@@ -14,9 +18,11 @@ insert into users_authorities(user_id, authorities_role) values('admin', 'admin'
 
 insert into users(id, password, enabled) values('manmay.mohanty@reverside.co.za', 'Password123', true);
 insert into users_authorities(user_id, authorities_role) values('manmay.mohanty@reverside.co.za', 'developer');
+insert into users_authorities(user_id, authorities_role) values('manmay.mohanty@reverside.co.za', 'notification_user');
+
 
 insert into clients(id, name, secret, owner, access_token_timeout, refresh_token_timeout) values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'reverside-calendar-service', '1498562418003', 'manmay.mohanty@reverside.co.za', 3600, -1);
-insert into clients_authorities(client_id, authorities_role) values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'system');
-insert into clients_authorities(client_id, authorities_role) values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'notification_user');
-insert into clients_authorities(client_id, authorities_role) values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'calendar_admin');
-insert into clients_grant_types(client_id, grant_types_grant) values('b094f68d-623a-4407-b0d2-b43c610b2c5','client_credentials');
+insert into clients_authorities(client_id, authorities_role)  values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'system');
+insert into clients_authorities(client_id, authorities_role)  values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'notification_user');
+insert into clients_authorities(client_id, authorities_role)  values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'calendar_admin');
+insert into clients_grant_types(client_id, grant_types_grant) values('b094f68d-623a-4407-b0d2-b43c610b2c5', 'implicit');
