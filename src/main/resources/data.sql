c20008d4212f2ed105c7fa1753468618e551b8af
Manmay <manmay.e.mohanty@gmail.com>
2017-09-25 21:27:23 +0200

    Custome login and registration page addition

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,12 +8,15 @@ insert into authorities(role) values('ADMIN');
 insert into authorities(role) values('SYSTEM');
 insert into authorities(role) values('DEVELOPER');
 
+
+insert into users(id, email, password, enabled) values(0, 'admin', 'admin', true);
+insert into users_authorities(user_id, authorities_role) values(0, 'USER');
+insert into users_authorities(user_id, authorities_role) values(0, 'ADMIN');
+
 insert into authorities(role) values('CONNECT_ADMIN');
 insert into authorities(role) values('CONNECT_USER');
 insert into authorities(role) values('CALENDAR_ADMIN');
 
-insert into users(id, name, password, enabled) values(0, 'admin', 'secret', true);
-insert into users_authorities(user_id, authorities_role) values(0, 'ADMIN');
 insert into users_authorities(user_id, authorities_role) values(0, 'CONNECT_USER');
 insert into users_authorities(user_id, authorities_role) values(0, 'CONNECT_ADMIN');
 insert into users_authorities(user_id, authorities_role) values(0, 'CALENDAR_ADMIN');
@@ -22,16 +25,7 @@ insert into clients(id, name, secret, owner, access_token_timeout, refresh_token
 insert into clients_grant_types(client_id, grant_types_grant) values(0, 'implicit');
 insert into clients_authorities(client_id, authorities_role)  values(0, 'SYSTEM');
 
-insert into users(id, name, password, enabled) values(1, 'manmay.mohanty@reverside.co.za', 'Password123', true);
-insert into users_authorities(user_id, authorities_role) values(1, 'USER');
-insert into users_authorities(user_id, authorities_role) values(1, 'CONNECT_USER');
-
-insert into users(id, name, password, enabled) values(2, 'cresundra.alves@reverside.co.za', 'Password123', true);
-insert into users_authorities(user_id, authorities_role) values(2, 'USER');
-insert into users_authorities(user_id, authorities_role) values(2, 'CONNECT_ADMIN');
-insert into users_authorities(user_id, authorities_role) values(2, 'CALENDAR_ADMIN');
-
-insert into clients(id, name, secret, owner, access_token_timeout, refresh_token_timeout) values(1, 'reverside-calendar-service', '1111111111111', 'manmay.mohanty@reverside.co.za', 3600, -1);
+insert into clients(id, name, secret, owner, access_token_timeout, refresh_token_timeout) values(1, 'reverside-calendar-service', '1111111111111', 'admin', 3600, -1);
 insert into clients_grant_types(client_id, grant_types_grant) values(1, 'client_credentials');
 insert into clients_authorities(client_id, authorities_role)  values(1, 'SYSTEM');
 insert into clients_authorities(client_id, authorities_role)  values(1, 'CONNECT_USER');
